4bd33877745b3a2c3fab52df741ce3adeca09eb3
Oshadha_Sam <samarakoonf@gmail.com>
2025-04-25 06:17:29 +0530

    Added Few Images

--- Week_00_C_Bootcamp_Python_to_C/03_printf.c
+++ Week_00_C_Bootcamp_Python_to_C/03_printf.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
 int main(){
+    printf(" \n");
+    printf("--------------------------------------- \n");
+    printf("Intro to printf\n");
+    printf("--------------------------------------- \n");
+    printf(" \n");
+
+    // Basic printf usage
+    printf("1. Basic printf usage\n");
+    printf(" \n");
+
+    printf("Hello, World!\n"); // Output a string
+    printf("Hello, %s\n", "World"); // Output a formatted string 
+    
+    
+    printf(" \n");
+
+    // Format Specifiers
+    printf("2. Format Specifiers\n");
+    printf(" \n");
+
     char name[] = "Gamma";
     int age = 19;
 
@@ -10,7 +30,8 @@ int main(){
 
 
     // Width and Precision Formatting
-    printf("width and Precision Formatting\n");
+    printf("3. width and Precision Formatting\n");
+    printf(" \n");
 
     float pi = 3.14159;
     printf("%.2f\n",pi);   // 3.14
@@ -22,7 +43,9 @@ int main(){
 
 
     //Padding and Alignment
-    printf("Padding and Alignment\n");
+    printf("4. Padding and Alignment\n");
+    printf(" \n");
+
 
 
     printf("%05d\n", 42); // 00042
@@ -36,16 +59,21 @@ int main(){
 
 
     //Hex Dumps
-    printf("Hex Dumps\n");
+    printf("5. Hex Dumps\n");
+    printf(" \n");
 
     int x= 255;
     printf("Hex: 0x%X\n",x);
 
+    printf(" \n");
+
     //Print Pointers
-    printf("Print Pointers\n"); 
+    printf("6. Print Pointers\n"); 
+    printf(" \n");
 
     int a= 43;
     printf("Address of a: %p\n",(void*)&a); // %p for pointer address
+    printf(" \n");
 
     return 0;
 
