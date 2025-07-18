97ed0eb84c72946db4bf38c3ef6efead0546901b
Oshadha_Sam <samarakoonf@gmail.com>
2025-07-18 17:14:19 +0530

    Add C example source and executables
    Added examples.c demonstrating character and string printing in C, along with compiled executables Exaaample.exe and examples.exe.

---
+++ Week_00_C_Bootcamp_Python_to_C/examples.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    printf("\'a\'");
+    printf("\'b\'");
+    printf("\'c\'");
+
+    printf("\n");
+
+    printf("\"A\"");
+    printf("\"B\"");
+    printf("\"C\"");    
+    return 0;
+}
