baf3c4c3206d29b20dcf48b6220f88145148212d
Oshadha_Sam <samarakoonf@gmail.com>
2025-07-19 11:42:31 +0530

    Add second button and callback to GTK example
    Introduced a second button to the GTK window and connected both buttons to a new print_hello callback function that prints a message when clicked. Refactored layout to use a vertical box container for better organization.

--- Week_02_GUI_programming_with_gtk/button.c
+++ Week_02_GUI_programming_with_gtk/button.c
@@ -1,9 +1,18 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
+
+//call bakc function to be executed when the button is clicked
+
+static void print_hello(GtkWidget *widget, gpointer data) {
+    printf("Hola, You Clicked the Button!\n");
+}
 
 
 static void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window;
-    GtkWidget *button;
+    GtkWidget *button1;
+    GtkWidget *button2;
+    GtkWidget *box;
 
     //create a new window and associate it with the application
     window = gtk_application_window_new(app);
@@ -11,13 +20,28 @@ static void activate(GtkApplication *app, gpointer user_data) {
     gtk_window_set_title(GTK_WINDOW(window), "Window with Button");
     gtk_window_set_default_size(GTK_WINDOW(window), 400,300);
 
+    //creat a vertical box with 5 pixels of spacing between children
+    box = gtk_box_new(GTK_ORIENTATION_VERTICAL,5);
+
+    //set the box as the child of the window
+    gtk_window_set_child(GTK_WINDOW(window),box);
+
+    //button 1
+
     //create a button with a label
-    button = gtk_button_new_with_label("Click Me");
+    button1 = gtk_button_new_with_label("Click Me");
+    //connect the button's clicked signal to the print_hello function
+    g_signal_connect(button1, "clicked", G_CALLBACK(print_hello), NULL);
+    //add the first button to the box
+    gtk_box_append(GTK_BOX(box),button1);
 
-    //set the button as the child of the window
-    gtk_window_set_child(GTK_WINDOW(window),button);
+    //button 2
 
-    //show the window
+    button2 = gtk_button_new_with_label("Me too");
+    g_signal_connect(button2, "clicked",G_CALLBACK(print_hello),NULL);
+    gtk_box_append(GTK_BOX(box),button2);
+
+
     gtk_window_present(GTK_WINDOW(window));
 }
 
