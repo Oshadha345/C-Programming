efec636c9921e70c0c30389ba7d1a164e175aae4
Oshadha_Sam <samarakoonf@gmail.com>
2025-07-23 17:16:08 +0530

    Add examples for extern and static variable usage in C
    Added source and executable files demonstrating the use of extern variables across multiple files and static-like behavior with a header-defined increment function. These examples illustrate variable linkage and scope in C for educational purposes.

---
+++ Week_01_Variables, Memory, and the Hidden Machinery of C/static/add.h
@@ -0,0 +1,13 @@
+#ifndef ADD_H
+#define ADD_H
+
+int count;
+
+int increment() {
+
+   
+    count = count +1;
+    return count;
+}
+
+#endif // ADD_H
---
+++ Week_01_Variables, Memory, and the Hidden Machinery of C/static/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "add.h"
+
+int main() {
+
+    int value;
+
+    value = increment();
+    value = increment();
+    value = increment();
+
+    printf("Final value: %d\n", value);
+
+    return 0;
+
+}
